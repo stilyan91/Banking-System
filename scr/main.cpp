33b06d536a2ebd0b020955a223fde3b1cfdb6505
stilyan91 <stilyan91@gmail.com>
2024-03-05 09:58:58 +0200

    extend Account

--- scr/main.cpp
+++ scr/main.cpp
@@ -4,10 +4,42 @@
 
 int main() {
 	Account acc(std::cin);
-	std::cout << acc.getBalance() << std::endl;
-	acc.deposit(10);
-	std::cout << acc.getBalance() << std::endl;
-	acc.withdraw(2);
-	std::cout<<acc.getBalance() << std::endl;
+	acc.getBalance();
+	acc.deposit(std::cin);
+	acc.getBalance();
+	acc.withdraw(std::cin);
+	acc.getBalance();
 }
 
+// Main Functionality Class
+/*
+
+class BankingSystem {
+public:
+    void run();
+    void createAccount();
+    void deposit();
+    void withdraw();
+    void closeAccount();
+    void balanceEnquiry();
+private:
+    // Add necessary data members and helper functions
+};
+
+// Account Class
+class Account {
+public:
+    void deposit(double amount);
+    void withdraw(double amount);
+    double getBalance() const;
+private:
+    // Add necessary data members
+};
+
+// Main Function
+int main() {
+    BankingSystem bankingSystem;
+    bankingSystem.run();
+    return 0;
+}
+*/
